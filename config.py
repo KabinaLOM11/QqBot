5b88e9fdffbe2ffcbce787d31dfcfb97ca626194
QQ <shutovich72@mail.ru>
2022-12-20 00:28:19 +0600

    QQBOT

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = "MTA1NDQyODEyNDI4NjI5MjAyOQ.GnQFg7.bb_8lro19-v9fW17uqhK8RiPwRNNDsQqOTXh60"  # Your Api Key
+ID_POST = 1037401096827719774  # Your Post ID
+USER_ROLES_LIST = ()
+MAX_ROLES = 20
+
+ROLES_LIST = {  # Your roles ID and emoji
+    "🐍": 1037397864818679869,  # Python
+    "💿": 1037397875182809129,  # PHP
+    "🎮": 1037397901216849991,  # C++
+    "📒": 1054432166056050808,  # c#    
+    "💻": 1037372759635918909,  # frontend 
+    "♂️": 1054454954636619877, # male
+    "♀️": 1054456891029344446, # famale
+}
+
